c7e63fdd765877c2576210a02472d154b488bbb6
CharanTeja-6825 <rcharanteja2006@gmail.com>
2025-11-26 11:57:23 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import dayjs from 'dayjs'
+import { navLinks, navIcons } from '#constants'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <img src="/images/logo.svg" alt='logo'/>
+            <p className='font-bold'>Charan's Portfolio</p>
+
+            <ul>
+                {navLinks.map(({id, name}) => (
+                    <li key={id}>{name}</li>
+                ))}
+            </ul>
+        </div>
+        <div>
+            <ul>
+                {navIcons.map(({id, img}) => (
+                    <li key={id}><img src={img} alt="hover-icons" className='icon-hover'/></li>
+                ))}
+            </ul>
+            <time>{dayjs().format('ddd D MMM h:mm A')}</time>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
