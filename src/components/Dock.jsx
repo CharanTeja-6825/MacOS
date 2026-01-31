22d9be3f181b817082df94bffec0a55b5f6c77cf
Rathikindi Charan Teja <rcharanteja2006@gmail.com>
2026-01-31 22:24:14 +0530

    mac-portfolio updates

---
+++ src/components/Dock.jsx
@@ -0,0 +1,30 @@
+import { dockApps } from '#constants';
+import React, { useRef } from 'react'
+
+const Dock = () => {
+    const dockRef = useRef(null);
+  const toggleApp = (app) => {};
+  return (
+    <section id='dock'>
+        <div ref={dockRef} className='dock-container'>
+            {dockApps.map(({id, name, icon, canOpen}) => (
+                <div key={id} className='relative flex justify-center'>
+                    <button 
+                    type='button' 
+                    className='dock-icon' 
+                    aria-label={name}
+                    data-tooltip-id = 'dock-tooltip'
+                    data-tooltip-content = {name}
+                    data-tooltip-delay-show={150}
+                    disabled={!canOpen}
+                    onClick={() => toggleApp({id, canOpen})}
+                    ></button>
+                    <img src={`/images/${icon}`} alt={name} loading='lazy' className={canOpen ? '' : 'opactiy-60'} width={100}/>
+                </div>
+            ))}
+        </div>
+    </section>
+  )
+}
+
+export default Dock
